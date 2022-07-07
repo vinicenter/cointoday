610d635f53c02341b033ce8f3edae79b00a3823d
vinicenter <viniemidiobosi@gmail.com>
2022-07-07 14:15:01 -0300

    feat: add datalist component

---
+++ src/components/core/form/DataListComponent.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export default {
+  props: {
+    id: {
+      type: String,
+      default: '',
+    },
+    list: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<template>
+  <datalist :id="id">
+    <option
+      v-for="(name, value) in list"
+      :key="name"
+      :value="value"
+    >
+      {{ name }}
+    </option>
+  </datalist>
+</template>
