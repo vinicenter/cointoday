040d099ff8eaa2f2b0a6d387f108494e8bca35b2
vinicenter <viniemidiobosi@gmail.com>
2022-07-08 21:16:57 -0300

    build: vite config resolve sintax errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import vue from '@vitejs/plugin-vue';
+
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslint()],
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
