28699b5308240d2982831ff45d4f84394e98fab0
vinicenter <viniemidiobosi@gmail.com>
2022-07-13 20:15:28 -0300

    style: fix linter errors on vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
-import eslint from 'vite-plugin-eslint';
 import vue from '@vitejs/plugin-vue';
+import eslint from 'vite-plugin-eslint';
 
 const path = require('path');
 
 export default defineConfig({
-  plugins: [vue(), eslint()],
+  plugins: [
+    vue(),
+    eslint(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
