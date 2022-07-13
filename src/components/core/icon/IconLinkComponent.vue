73b63aed7c673d637866e437379466a87810308c
vinicenter <viniemidiobosi@gmail.com>
2022-07-13 19:56:13 -0300

    feat: add icon component package and IconLinkComponent

---
+++ src/components/core/icon/IconLinkComponent.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import IconComponent from '@/components/core/icon/IconComponent.vue';
+</script>
+
+<script lang="ts">
+export default {
+  props: {
+    href: String,
+    icon: String,
+    size: Number,
+  },
+  components: {
+    IconComponent,
+  },
+};
+</script>
+
+<template>
+  <a
+    :href="href"
+    class="text-decoration-none"
+    target="_blank"
+    tabindex="0"
+  >
+    <IconComponent
+      :icon="icon"
+      :size="size"
+    />
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+  color: var(--textColor);
+}
+.icon:hover {
+  color: var(--textColorHover);
+  -webkit-transition: color 0.15s ease, color 0.15s ease;
+  transition: color 0.15s ease, color 0.15s ease;
+}
+</style>
